0d0b754b4690c72b3dbd274c62fac21e9b851326
sadafnazir-lab <69804253+sadafnazir-lab@users.noreply.github.com>
2020-09-02 13:54:07 +0530

    Create posttest.js

---
+++ Experiment/posttest.js
@@ -0,0 +1,19 @@
+
+# **5.Post Test**
+
+1. Crystal violet is a \_\_\_\_\_\_\_\_\_ stain
+
+**a) primary stain** b) secondary stain c) tertiary stain d)quarternary stain
+
+2. Ethanol is a \_\_\_\_\_\_\_\_\_\_ agent
+
+a)mordant **b)decolorizing agent** c)acid dye d) basic dye
+
+3. Safranin is \_\_\_\_\_\_\_\_\_
+
+**a) counterstain** b)primary stain c)both a &amp; b d) None
+
+4. Acid stains are\_\_\_\_\_\_\_\_
+
+a)Methylene blue, crystal violet, malachite green, basic fuschsin, carbolfuschsin, safranin b) **Eosine, acid fuchsin, rose bengal, Congo red** c)both a &amp; b d) None
+
