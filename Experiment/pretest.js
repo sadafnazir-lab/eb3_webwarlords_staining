956cd88277add4261d3d5ee7c453a57de877d0be
sadafnazir-lab <69804253+sadafnazir-lab@users.noreply.github.com>
2020-09-02 13:53:33 +0530

    Create pretest.js

---
+++ Experiment/pretest.js
@@ -0,0 +1,92 @@
+
+# **4.Pre Test**
+
+1. The percent of alcohol used in staining in gram staining is
+
+A. 75%
+
+B. 60%
+
+C. 25%
+
+**D. 90%**
+
+2. Methylene blue is a
+
+A. Acidic dye
+
+B. Basic dye
+
+C. Oxazine dye
+
+**D. Neutral dye**
+
+3. . If only one stain is used for staining a specimen
+
+**A. Simple staining**
+
+B. Negative staining
+
+C. Differential staining
+
+D. None of these
+
+4. Other than the sample (specimen) the remaining portion is stained then it is called
+
+a. Simple staining
+
+b. Negative staining
+
+**c. Differential staining**
+
+d. None of these
+
+5. Gram positive bacteria appear as
+
+A. Pink
+
+**B. Voilet**
+
+C. None
+
+D. Both A &amp; B
+
+6. The action of alcohol during Gram staining-
+
+A. Allows the colour
+
+B. It adds colour
+
+**C. Decolourises the cells**
+
+D. None of these
+
+7. Gram negative bacteria appear as
+
+**A. Pink**
+
+B. Voilet
+
+C. None
+
+D. Both A &amp; B
+
+8. Gram positive bacteria are usually susceptible to
+
+A. Streptomycin
+
+B. Tetracyclin
+
+**C. Penicillin**
+
+D. Ampicilliin
+
+9. During staining for Electron Microscopy, the method which improves contrast of specimen is
+
+a. Positive staining
+
+**b. Negative staining**
+
+c. Shadow staining
+
+d. None of these
